6731c960e8a7a6b2b3891e91486ab017286519a2
DayoOliyide <dayo.oliyide@gmail.com>
2018-11-20 17:42:27 +0000

    Creating the test stream and publishing onto it more quietly

--- publish-a-tale.sh
+++ publish-a-tale.sh
@@ -4,8 +4,11 @@ AWS_DEFAULT_REGION=eu-west-1
 AWS_ACCESS_KEY_ID=blah
 AWS_SECRET_ACCESS_KEY=doubleblah
 
+KINESIS_STREAM=Tales
+KINESIS_SHARDS=10
 
-A_TALE="it was the worst of times,
+A_TALE="It was the best of times,
+it was the worst of times,
 it was the age of wisdom,
 it was the age of foolishness,
 it was the epoch of belief,
@@ -19,10 +22,14 @@ we had nothing before us,
 we were all going direct to Heaven,
 we were all going direct the other way"
 
+printf "Creating a kinesis stream called %s with %d shards .." ${KINESIS_STREAM} ${KINESIS_SHARDS}
+aws --endpoint-url=http://localhost:4568 kinesis create-stream --stream-name ${KINESIS_STREAM} --shard-count ${KINESIS_SHARDS}
+printf ".. Created stream \n" ${KINESIS_STREAM} ${KINESIS_SHARDS}
 
-
+printf "Publishing a Tale on %s " ${KINESIS_STREAM}
 while read -r line
 do
-    aws --endpoint-url=http://localhost:4568 kinesis put-record --stream-name Tales --partition-key $(($RANDOM % 1000)) --data "$line"
+    aws --endpoint-url=http://localhost:4568 kinesis put-record --stream-name ${KINESIS_STREAM} --partition-key $(($RANDOM % ${KINESIS_SHARDS})) --data "$line" 1> /dev/null
+    printf "."
 done < <(printf '%s\n' "$A_TALE")
-
+printf " Finished publishing\n"
